ab47564540f955176e05e70ce8b2e5f368e83173
Miguel Caballero <caballero.mc98@gmail.com>
2022-10-10 16:52:01 -0500

    added validate subsequence

--- Easy/ValidateSubsequence/ValidateSubsequence.py
+++ Easy/ValidateSubsequence/ValidateSubsequence.py
@@ -1,1 +1,8 @@
- 
+def soluton1(s, t):
+    t_ptr = 0
+    s_ptr = 0
+    while t_ptr < len(t) and s_ptr < len(s):
+        if s[s_ptr] == t[t_ptr]:
+            s_ptr += 1
+        t_ptr += 1
+    return len(s) == s_ptr

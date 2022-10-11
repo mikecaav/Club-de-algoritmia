36e76d3a128a7c79cd74035f86c118c7bcf975da
miguelcaballero <caballero.mc98@gmail.com>
2022-10-11 16:46:05 -0500

    added sorted squared arrays

--- Easy/SortedSquaredArray/SortedSquaredArray.py
+++ Easy/SortedSquaredArray/SortedSquaredArray.py
@@ -1,1 +1,15 @@
- 
+def solution1(nums):
+    return sorted([value**2 for value in nums])
+
+
+def solution2(nums):
+    res = []
+    l, r = 0, len(nums) - 1
+    while l <= r:
+        if nums[l] ** 2 > nums[r] ** 2:
+            res.append(nums[l] ** 2)
+            l += 1
+        else:
+            res.append(nums[r] ** 2)
+            r -= 1
+    return res[::-1]

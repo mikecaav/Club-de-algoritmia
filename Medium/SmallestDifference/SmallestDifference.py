97f2020156fe527cc7d65b27a3d1b2bcadd9d112
miguelcaballero <caballero.mc98@gmail.com>
2022-10-12 17:06:42 -0500

    added smallest difference python

--- Medium/SmallestDifference/SmallestDifference.py
+++ Medium/SmallestDifference/SmallestDifference.py
@@ -1,1 +1,13 @@
- 
+def solution1(arr):
+    results = []
+    minimal = float('inf')
+    arr.sort()
+
+    for (a, b) in zip(arr, arr[1:]):
+        diff = abs(b - a)
+        if diff == minimal:
+            results.append((a, b))
+        elif diff < minimal:
+            results = [(a, b)]
+
+    return results

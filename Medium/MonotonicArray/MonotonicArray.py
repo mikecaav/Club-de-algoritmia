40fc484d038640d0b4d935598bb24283f313e63b
miguelcaballero <caballero.mc98@gmail.com>
2022-10-13 11:59:22 -0600

    added monotonic array in python

--- Medium/MonotonicArray/MonotonicArray.py
+++ Medium/MonotonicArray/MonotonicArray.py
@@ -1,1 +1,9 @@
- 
+def solution1(nums):
+    inc = True
+    dec = True
+    for i in range(1, len(nums)):
+        if nums[i] > nums[i-1]:
+            dec = False
+        if nums[i] < nums[i-1]:
+            inc = False
+    return inc or dec

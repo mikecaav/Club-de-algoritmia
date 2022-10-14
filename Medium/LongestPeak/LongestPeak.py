166a4820171e284c0d4c4cc2fc44140fee22f3c1
Miguel Caballero <caballero.mc98@gmail.com>
2022-10-14 14:00:51 -0500

    added longest peak in python

--- Medium/LongestPeak/LongestPeak.py
+++ Medium/LongestPeak/LongestPeak.py
@@ -1,1 +1,11 @@
- 
+def solution1(arr):
+    res = 0
+    for indx in range(1, len(arr) - 1):
+        if arr[indx - 1] < arr[indx] > arr[indx + 1]:
+            l = r = indx
+            while l > 0 and arr[l] > arr[l - 1]:
+                l -= 1
+            while r + 1 < len(arr) and arr[r] > arr[r + 1]:
+                r += 1
+            res = max(res, r - l + 1)
+    return res

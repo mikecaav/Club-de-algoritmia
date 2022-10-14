c1dc4ab0b90e7cc7f1f696d74db4378a5c880bb6
Miguel Caballero <caballero.mc98@gmail.com>
2022-10-14 14:19:12 -0500

    added array of products

--- Medium/ArrayOfProducts/ArrayOfProducts.py
+++ Medium/ArrayOfProducts/ArrayOfProducts.py
@@ -1,1 +1,12 @@
- 
+def solution1(nums):
+    res = [1] * len(nums)
+
+    prefix = 1
+    for i in range(len(nums)):
+        res[i] = prefix
+        prefix *= nums[i]
+    postFix = 1
+    for i in range(len(nums) - 1, -1, -1):
+        res[i] *= postFix
+        postFix *= nums[i]
+    return res
